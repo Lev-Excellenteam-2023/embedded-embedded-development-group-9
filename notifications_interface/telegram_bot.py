97049af6aa695b48ce946009d30f11893aef41db
rivkaadler1 <adlerrivka66@gmail.com>
2023-08-29 16:45:36 +0300

    need to fix the bug that it takes a very long time to get picture

--- notifications_interface/telegram_bot.py
+++ notifications_interface/telegram_bot.py
@@ -1,15 +1,17 @@
-import os
+# import os
 import requests
 from flask import Flask, request, Response
+import manager
+from Person_Detection import PersonDetection
 
-users = []
-
-API_URL = f'https://api.telegram.org/bot{os.getenv("TOKEN")}/'
-TELEGRAM_INIT_WEBHOOK_URL = API_URL + f'{os.getenv("SETWEBHOOK")}/message'
-
+chat_id_bounding_box_dict = {}
+TOKEN = '6413186718:AAHiFecdSbZRKg1rRkxhCbJomP9et4xLBT4'
+API_URL = 'https://api.telegram.org/bot6413186718:AAHiFecdSbZRKg1rRkxhCbJomP9et4xLBT4/'
+TELEGRAM_INIT_WEBHOOK_URL = API_URL + 'setWebhook?url=https://1f91-62-219-32-82.ngrok-free.app/message'
 
 requests.get(TELEGRAM_INIT_WEBHOOK_URL)
 app = Flask(__name__)
+in_register_progress = []
 
 
 @app.route('/message', methods=["POST"])
@@ -17,12 +19,17 @@ def handle_message():
     data = request.get_json()
     chat_id = data['message']['chat']['id']
     message_text = data['message']['text']
-    if message_text == '/start':
-        users.append(chat_id)
+    if message_text == '/start' and chat_id not in in_register_progress:
+        in_register_progress.append(chat_id)
+        print("hi")
+        image_path, bounding_boxes_dict = PersonDetection()
         send_message(chat_id, "Welcome to our SitSmart system, please choose the number of  your workspace from the "
                               "picture below")
-        send_photo(chat_id, open(r'IMG_0443.JPG', 'rb'))
-        send_message(chat_id, "")
+        send_photo(chat_id, open(image_path, 'rb'))
+        chat_id_bounding_box_dict[chat_id] = bounding_boxes_dict
+    elif message_text.isdigit():
+        manager.add_user(chat_id, chat_id_bounding_box_dict[chat_id][int(message_text)])
+        send_message(chat_id, "detection has been started...")
     return Response("success")
 
 
