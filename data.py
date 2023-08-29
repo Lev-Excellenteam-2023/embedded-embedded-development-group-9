dc5729034d72c8ca5d88d3052a07411b98627a67
TamariIv <taivgi@g.jct.ac.il>
2023-08-29 10:24:54 +0300

    add data file

---
+++ data.py
@@ -0,0 +1,18 @@
+from user import User
+
+users = []
+
+
+def add_user(user: User):
+    users.append(user)
+
+
+def get_user(chat_id: int) -> User:
+    for user in users:
+        if user.chat_id == chat_id:
+            return user
+    return None
+
+
+def get_users() -> list:
+    return users
